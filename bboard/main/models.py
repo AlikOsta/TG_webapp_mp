39abb09116f44e461d243d241e3b7c59463e7457
AlikOsta <167987940+AlikOsta@users.noreply.github.com>
2024-08-10 16:22:06 +0300

    Ostrovan: Создание models Bb and Rubric

--- bboard/main/models.py
+++ bboard/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Bb(models.Model):
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+    title = models.CharField(max_length=40, verbose_name='Товар')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.IntegerField(default=0, null=True, blank=True, verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+
+    def delete(self, *args, **kwargs):
+        for ai in self.additionalimage_set.all():
+            ai.delete()
+        super().delete(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Объявления"
+        verbose_name = "Объявление"
+        ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name="Название")
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Создание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Рубрики"
+        verbose_name = "Рубрика"
+        ordering = ['published']
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -2,25 +2,28 @@ from django.shortcuts import render
 
 
 def index(request):
-    data = {'title' : 'Главная страница'}
-    return render(request, 'main/index.html', data)
+    data = {
+        'title' : 'Главная страница',
+
+    }
+    return render(request, 'main/index.html', context=data)
 
 
 def by_rubric(request):
     data = {'title' : 'Рубрики'}
-    return render(request, 'main/by_rubric.html', data)
+    return render(request, 'main/by_rubric.html', context=data)
 
 
 def user(request):
     data = {'title' : 'Пользователь'}
-    return render(request, 'main/user.html', data)
+    return render(request, 'main/user.html', context=data)
 
 
 def detail(request):
     data = {'title' : 'Детали товара'}
-    return render(request, 'main/detail.html', data)
+    return render(request, 'main/detail.html', context=data)
 
 
 def create(request):
     data = {'title' : 'Новое объявление'}
-    return render(request, 'main/create.html', data)
+    return render(request, 'main/create.html', context=data)
